08383dad65edaff1139cc6bbf4fe079c0eb0f0fb
VanPark <vanpark212@gmail.com>
2023-03-17 09:50:35 -0700

    Got rid of auto sign-in

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -17,7 +17,6 @@ public class Driver {
      * @param args String[]
      */
     public static void main(String[] args) {
-        TutorWindow tutorWindow = new TutorWindow("asjbda");
         loginWindow = new LoginWindow();
         UserAccountDB db = new UserAccountDB();
         Blackboard.getInstance().setDb(db);
